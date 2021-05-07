bc304c1c5156b976e60fbc10ae6337a8a20b27eb
Alexander Zarkov <alexander.zarcoff@outlook.com>
2021-05-07 15:44:04 +0300

    added Coin components, css styles, API fetch functions

--- crypto-tracker/src/App.js
+++ crypto-tracker/src/App.js
@@ -1,9 +1,47 @@
 import './App.css';
+import axios from "axios";
+import {useState, useEffect} from "react";
+import Coin from "./components/Coin";
 
 function App() {
+
+    const [coins, setCoins] = useState([]);
+    const [search, setSearch] = useState('');
+
+    useEffect(() => {
+        axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+            .then(res => {
+                setCoins(res.data);
+                console.log(coins);
+        }).catch(err => alert(err))
+    }, []);
+
+    const handleChange = e => {
+        setSearch(e.target.value)
+    }
+
+    const filteredCoins = coins.filter(coin => coin.name.toLowerCase().includes(search.toLowerCase()));
+
   return (
-    <div className="App">
-      React Project
+    <div className="price-app">
+      <div className="coin-search">
+          <h1 className="coin-text">Search a currency</h1>
+          <form action="">
+              <input type="text" placeholder="Search..." className="coin-input" onChange={handleChange}/>
+          </form>
+      </div>
+        {filteredCoins.map(coin => {
+            return <Coin
+                key={coin.id}
+                name={coin.name}
+                image={coin.image}
+                symbol={coin.symbol}
+                volume={coin.total_volume}
+                price={coin.current_price}
+                priceChange={coin.price_change_percentage_24h}
+                marketCap={coin.market_cap}
+            />;
+        })}
     </div>
   );
 }
