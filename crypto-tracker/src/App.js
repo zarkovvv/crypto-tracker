c78c160dbf60083978fb3daecc7f6a3d5ce644c9
Alexander Zarkov <alexander.zarcoff@outlook.com>
2021-05-07 22:16:42 +0300

    added animations and loader component

--- crypto-tracker/src/App.js
+++ crypto-tracker/src/App.js
@@ -2,6 +2,9 @@ import './App.css';
 import axios from "axios";
 import {useState, useEffect} from "react";
 import Coin from "./components/Coin";
+import Loader from "./components/Loader";
+import {AnimatePresence, motion} from "framer-motion";
+import React from "react";
 
 function App() {
 
@@ -9,11 +12,14 @@ function App() {
     const [search, setSearch] = useState('');
 
     useEffect(() => {
-        axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1&sparkline=false')
-            .then(res => {
-                setCoins(res.data);
-                console.log(coins);
-        }).catch(err => alert(err))
+        async function fetchData() {
+            const result = await axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1&sparkline=false');
+            setCoins(result.data);
+        }
+        setTimeout(fetchData, 2000);
+        setInterval(async () => {
+            await fetchData();
+        }, 60000)
     }, []);
 
     const handleChange = e => {
@@ -22,28 +28,43 @@ function App() {
 
     const filteredCoins = coins.filter(coin => coin.name.toLowerCase().includes(search.toLowerCase()));
 
-  return (
-    <div className="price-app">
-      <div className="coin-search">
-          <h1 className="coin-text">Search a currency</h1>
-          <form action="">
-              <input type="text" placeholder="Search..." className="coin-input" onChange={handleChange}/>
-          </form>
-      </div>
-        {filteredCoins.map(coin => {
-            return <Coin
-                key={coin.id}
-                name={coin.name}
-                image={coin.image}
-                symbol={coin.symbol}
-                volume={coin.total_volume}
-                price={coin.current_price}
-                priceChange={coin.price_change_percentage_24h}
-                marketCap={coin.market_cap}
-            />;
-        })}
-    </div>
-  );
+    if (coins.length === 0) {
+        return (
+            <Loader />
+        );
+    } else {
+        return (
+            <AnimatePresence>
+                <motion.div
+                    exit={{opacity: 0}}
+                    initial={{ opacity: 0, y: 0 }}
+                    animate={{ opacity: 1, y: 0}}
+                >
+                    <div className="price-app">
+                        <div className="coin-search">
+                            <h1 className="coin-text">Search a currency</h1>
+                            <form action="">
+                                <input type="text" placeholder="Search..." className="coin-input" onChange={handleChange}/>
+                            </form>
+                        </div>
+                        {filteredCoins.map(coin => {
+                            return <Coin
+                                        key={coin.id}
+                                        name={coin.name}
+                                        image={coin.image}
+                                        symbol={coin.symbol}
+                                        volume={coin.total_volume}
+                                        price={coin.current_price}
+                                        priceChange={coin.price_change_percentage_24h}
+                                        marketCap={coin.market_cap}
+                                    />
+                            ;
+                        })}
+                    </div>
+                </motion.div>
+            </AnimatePresence>
+        );
+    }
 }
 
 export default App;
--- crypto-tracker/src/components/Coin.js
+++ crypto-tracker/src/components/Coin.js
@@ -1,25 +1,34 @@
 import React from 'react';
 import './Coin.css';
+import {AnimatePresence, motion} from "framer-motion";
 
 const Coin = ({name, image, symbol, price, volume, priceChange, marketCap}) => {
     return (
-        <div className="coin-container">
-            <div className="coin-row">
-                <div className="coin">
-                    <img src={image} alt="crypto"/>
-                    <h1>{name}</h1>
-                    <p className="coin-symbol">{symbol}</p>
+        <AnimatePresence>
+            <motion.div
+                exit={{opacity: 0}}
+                initial={{ opacity: 0, y: 0}}
+                animate={{ opacity: 1, y: 0}}
+            >
+                <div className="coin-container">
+                    <div className="coin-row">
+                        <div className="coin">
+                            <img src={image} alt="crypto"/>
+                            <h1>{name}</h1>
+                            <p className="coin-symbol">{symbol}</p>
+                        </div>
+                        <div className="coin-data">
+                            <p className="coin-price">${price}</p>
+                            <p className="coin-volume">${volume.toLocaleString()}</p>
+                            {priceChange < 0 ? (<p className="coin-percent red">{priceChange.toFixed(2)}%</p>) : (<p className="coin-percent green">{priceChange.toFixed(2)}%</p>)}
+                            <p className="coin-marketcap">
+                                Market Cap: ${marketCap.toLocaleString()}
+                            </p>
+                        </div>
+                    </div>
                 </div>
-                <div className="coin-data">
-                    <p className="coin-price">${price}</p>
-                    <p className="coin-volume">${volume.toLocaleString()}</p>
-                    {priceChange < 0 ? (<p className="coin-percent red">{priceChange.toFixed(2)}%</p>) : (<p className="coin-percent green">{priceChange.toFixed(2)}%</p>)}
-                    <p className="coin-marketcap">
-                        Market Cap: ${marketCap.toLocaleString()}
-                    </p>
-                </div>
-            </div>
-        </div>
+            </motion.div>
+        </AnimatePresence>
     );
 };
 
