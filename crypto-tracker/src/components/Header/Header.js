77091dce48f178444d85ee53247f19fffa32711b
Alexander Zarkov <alexander.zarcoff@outlook.com>
2021-05-28 17:00:05 +0300

    visual improvements

--- crypto-tracker/src/components/Header/Header.js
+++ crypto-tracker/src/components/Header/Header.js
@@ -3,6 +3,21 @@ import './Header.scss'
 import Navigation from "../Navigation/Navigation";
 
 const Header = () => {
+    window.onscroll = function(){
+        let top = window.scrollY;
+        console.log('Top: ' + top);
+        let header = document.getElementsByClassName('header');
+        let offset = header[0].clientHeight; //changed offset to be dynamic, so it works on mobile screens.
+        console.log(offset)
+        if(top > offset){
+            header[0].classList.remove('top');
+            header[0].classList.add('scrolled');
+        } else {
+            header[0].classList.remove('scrolled');
+            header[0].classList.add('top');
+        }
+    };
+
     return (
         <div className='header'>
             <Navigation />
