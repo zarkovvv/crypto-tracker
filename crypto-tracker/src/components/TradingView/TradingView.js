8bae3c472bb2255066e883c2a94f5190928ffe87
Alexander Zarkov <alexander.zarcoff@outlook.com>
2021-05-28 12:32:43 +0300

    added responsiveness to the elements

--- crypto-tracker/src/components/TradingView/TradingView.js
+++ crypto-tracker/src/components/TradingView/TradingView.js
@@ -7,7 +7,7 @@ const TradingView = () => {
             <div className="heading-container">
                 <h1 className='heading'>Trading View</h1>
             </div>
-            <div>
+            <div className='chart-container'>
                 <iframe id="tradingview_78c91"
                         title='trading-view'
                         src="https://s.tradingview.com/widgetembed/?frameElementId=tradingview_78c91&amp;symbol=BTCUSD&amp;interval=D&amp;hidesidetoolbar=1&amp;symboledit=1&amp;saveimage=1&amp;toolbarbg=F1F3F6&amp;studies=%5B%5D&amp;hideideas=1&amp;theme=Dark&amp;style=1&amp;timezone=Etc%2FUTC&amp;studies_overrides=%7B%7D&amp;overrides=%7B%7D&amp;enabled_features=%5B%5D&amp;disabled_features=%5B%5D&amp;locale=eu&amp;utm_medium=widget&amp;utm_campaign=chart&amp;"
